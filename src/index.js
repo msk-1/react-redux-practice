2570650a7d7e847191d9aa5210b1585ab64347e7
msk1 <48862098+msk-1@users.noreply.github.com>
2019-12-18 01:23:34 +0900

    fix props

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetails"
+import CommentDetail from "./CommentDetails";
+import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:23PM" content="Nice blog post" />
-      <CommentDetail author="Alex" timeAgo="Today at 3:33PM" content="I like the subject" />
-      <CommentDetail author="Jane" timeAgo="Today at 6:43PM" content="I like the writing" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:23PM"
+        content="Nice blog post"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 3:33PM"
+        content="I like the subject"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Today at 6:43PM"
+        content="I like the writing"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
